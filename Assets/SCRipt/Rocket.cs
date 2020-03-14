170ffc82abb4ce0e3b8b4161500ed0fb38277ca0
jjeepjoe <jjeepjoe@hotmail.com>
2020-03-13 20:17:16 -0500

    got up motion

---
+++ Assets/SCRipt/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    //CONFIG PARAMS
+
+    //CACHE/ HANDLEs
+    Rigidbody myRB;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+    //keep it clean
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting pressed");
+            myRB.AddRelativeForce(Vector3.up);
+        }
+        //This will only allow a single press to be processed. The top is the boss if both pressed.
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotate right");
+        }
+    }
+}
